aec05a664a3a7e73ece190abfbc7a317f47b0866
Obiora Amobi <obiora.amobi@gmail.com>
2016-05-04 09:25:14 +0100

    updates

--- the_mma_times/urls.py
+++ the_mma_times/urls.py
@@ -20,6 +20,7 @@ from contact_app import views as contact_views
 from .settings import MEDIA_ROOT
 from django.views.static import serve
 from accounts_app import views as accounts_views
+from django.contrib.staticfiles import views as static_views
 
 
 urlpatterns = [
@@ -33,4 +34,7 @@ urlpatterns = [
 
     # Media root for images
     url(r'^media/(?P<path>.*)$', serve, {'document_root': MEDIA_ROOT}),
+
+    #Static files for Heroku
+    url(r'^static/(?P<path>.*)$', static_views.serve),
 ]
